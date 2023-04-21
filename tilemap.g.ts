465afea2a3d1c0e0d6f74ead7a9b292e4cb2b5b6
Valis World <124701656+Valis-World@users.noreply.github.com>
2023-04-21 14:31:04 +0200

    Sprite animiert

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,4 +1,4 @@
-// Auto-generated code. Do not edit.
+// Automatisch generierter Code. Nicht bearbeiten.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
@@ -36,4 +36,4 @@ namespace myTiles {
     })
 
 }
-// Auto-generated code. Do not edit.
+// Automatisch generierter Code. Nicht bearbeiten.
